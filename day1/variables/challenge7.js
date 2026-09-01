32fc4dcefa751e5577c24085d15be9c98ec445f9
baqqali safaa <bql.sf2001@gmail.com>
2026-09-01 14:43:52 +0100

    finir le challenge7

--- day1/variables/challenge7.js
+++ day1/variables/challenge7.js
@@ -1,2 +1,13 @@
 const prompt = require ("prompt-sync")();
-let cntrl = prompt("")
+
+let noteCC = prompt("Contrôle continu : " );
+let noteProjet = prompt("Projet : " );
+let noteExamen = prompt("Exam final : " );
+
+console.log("Contrôle continu : " + noteCC);
+console.log("Projet : " + noteProjet);
+console.log("Examen : " + noteExamen);
+
+let noteFinale = (noteCC * 2 + noteProjet * 3 + noteExamen * 5) / 10
+
+console.log("Note finale : " + noteFinale);
