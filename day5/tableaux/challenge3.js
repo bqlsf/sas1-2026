9e706a45e9c9afab1deb896f6832b18c7e8c60e9
baqqali safaa <bql.sf2001@gmail.com>
2026-09-07 10:46:08 +0100

    challenge

---
+++ day5/tableaux/challenge3.js
@@ -0,0 +1,13 @@
+function trouverMax(tab){
+
+    let max = tab[0];
+    for(let i = 1 ; i < tab.length; i++){
+        if(max < tab[i]){
+            max = tab[i];
+        }
+        }
+    console.log(max);
+}
+
+
+trouverMax([1, 10, 9, 5]);
